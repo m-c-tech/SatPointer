72a31d3efd77c9c2cf707c62bf5b20a2ad90d552
m-c-tech <73830912+m-c-tech@users.noreply.github.com>
2025-01-21 00:34:51 +0800

    Added BNO085 sensor feedback
    Using an IMU to give real world absolute position in relation to gravity and magnetic north

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # SatPointer
- 
+
 Takes commands from rotctl or rotctld and points an antenna.
+Uses a BNO085 IMU mounted on the antenna to detect the absolute rotation compared to gravity and magnetic north.
+PID control closes the loop between the motor and IMU.
+
+Example roctld command:
+.\rotctld.exe -m 204 -r COM1 -s 115200 -T 127.0.0.1 -t 4533
+
+Satdump should be able to connect and command the device with the above ip and port.
--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,13 @@
 #include <Servo.h>
 #include <SoftwareSerial.h>
 #include <Adafruit_BNO08x.h>
+#include <QuickPID.h>
 
 #define debugSerial_TX 17
 #define debugSerial_RX 18
+#define BNO08X_SDA 21
+#define BNO08X_SCL 22
+#define BNO08X_RESET -1
 
 float azimuth = 0.0;  // Store the current azimuth
 float elevation = 0.0; // Store the current elevation
@@ -12,14 +16,14 @@ String inputString = "";      // a String to hold incoming data
 bool stringComplete = false;  // whether the string is complete
 bool hasBNO08X = false;
 static const int servoPin = 16;
+float outputServoAngle = 0;
+float desiredAzimuthAngle = 45, sensorAzimuthAngle = 0, outputAzimuthPID = 0, Kp = 0.1, Ki = 0.8, Kd = 0;
 
-Servo servo1;
-EspSoftwareSerial::UART debugSerial;
-
-#define BNO08X_SDA 21
-#define BNO08X_SCL 22
-#define BNO08X_RESET -1
-
+QuickPID azimuthPID(&sensorAzimuthAngle, &outputAzimuthPID, &desiredAzimuthAngle, Kp, Ki, Kd,  /* OPTIONS */
+               azimuthPID.pMode::pOnError,                   /* pOnError, pOnMeas, pOnErrorMeas */
+               azimuthPID.dMode::dOnMeas,                    /* dOnError, dOnMeas */
+               azimuthPID.iAwMode::iAwCondition,             /* iAwCondition, iAwClamp, iAwOff */
+               azimuthPID.Action::direct); 
 
 struct euler_t {
   float yaw;
@@ -27,15 +31,17 @@ struct euler_t {
   float roll;
 } ypr;
 
+Servo servo1;
+EspSoftwareSerial::UART debugSerial;
 Adafruit_BNO08x  bno08x(BNO08X_RESET);
 sh2_SensorValue_t sensorValue;
-sh2_SensorId_t reportType = SH2_ARVR_STABILIZED_RV;
-long reportIntervalUs = 5000;
+sh2_SensorId_t reportType = SH2_ROTATION_VECTOR;
+long reportIMUIntervalUs = 10000;
 
 void setReports(sh2_SensorId_t reportType, long report_interval) {
-  Serial.println("Setting desired reports");
+  debugSerial.println("Setting desired reports");
   if (!bno08x.enableReport(reportType, report_interval)) {
-    Serial.println("Could not enable stabilized remote vector");
+    debugSerial.println("Could not enable stabilized remote vector");
   }
 }
 
@@ -55,43 +61,14 @@ void setup() {
   } else {
     debugSerial.println("BNO08x started");
     hasBNO08X = true;
-    for (int n = 0; n < bno08x.prodIds.numEntries; n++) {
-      debugSerial.print("Part ");
-      debugSerial.print(bno08x.prodIds.entry[n].swPartNumber);
-      debugSerial.print(": Version :");
-      debugSerial.print(bno08x.prodIds.entry[n].swVersionMajor);
-      debugSerial.print(".");
-      debugSerial.print(bno08x.prodIds.entry[n].swVersionMinor);
-      debugSerial.print(".");
-      debugSerial.print(bno08x.prodIds.entry[n].swVersionPatch);
-      debugSerial.print(" Build ");
-      debugSerial.println(bno08x.prodIds.entry[n].swBuildNumber);
-      setReports(reportType, reportIntervalUs);
-    }
+    setReports(reportType, reportIMUIntervalUs);
   }
+  azimuthPID.SetOutputLimits(0, 180);
+  azimuthPID.SetSampleTimeUs(100000);
+  azimuthPID.SetTunings(Kp, Ki, Kd);
+  azimuthPID.SetMode(azimuthPID.Control::automatic);
   inputString.reserve(200);
-  servo1.attach(
-      servoPin,
-      Servo::CHANNEL_NOT_ATTACHED,
-      0,
-      120);
-}
-
-void runStepper()
-{
-  for (int posDegrees = 0; posDegrees <= 180; posDegrees++)
-  {
-    servo1.write(posDegrees);
-    Serial.println(posDegrees);
-    delay(20);
-  }
-
-  for (int posDegrees = 180; posDegrees >= 0; posDegrees--)
-  {
-    servo1.write(posDegrees);
-    Serial.println(posDegrees);
-    delay(20);
-  }
+  servo1.attach(servoPin, Servo::CHANNEL_NOT_ATTACHED, 0, 180);
 }
 
 void quaternionToEuler(float qr, float qi, float qj, float qk, euler_t* ypr, bool degrees = false) {
@@ -123,27 +100,19 @@ void quaternionToEulerGI(sh2_GyroIntegratedRV_t* rotational_vector, euler_t* ypr
 void loop() {
   if (bno08x.wasReset()) {
     debugSerial.print("sensor was reset ");
-    setReports(reportType, reportIntervalUs);
+    setReports(reportType, reportIMUIntervalUs);
   }
+
   if (bno08x.getSensorEvent(&sensorValue)) {
-    // in this demo only one report type will be received depending on FAST_MODE define (above)
-    switch (sensorValue.sensorId) {
-      case SH2_ARVR_STABILIZED_RV:
-        quaternionToEulerRV(&sensorValue.un.arvrStabilizedRV, &ypr, true);
-      case SH2_GYRO_INTEGRATED_RV:
-        // faster (more noise?)
-        quaternionToEulerGI(&sensorValue.un.gyroIntegratedRV, &ypr, true);
-        break;
-    }
-    static long last = 0;
-    long now = micros();
-    debugSerial.print(now - last);             Serial.print("\t");
-    last = now;
-    debugSerial.print(sensorValue.status);     Serial.print("\t");  // This is accuracy in the range of 0 to 3
-    debugSerial.print(ypr.yaw);                Serial.print("\t");
-    debugSerial.print(ypr.pitch);              Serial.print("\t");
-    debugSerial.println(ypr.roll);
+    quaternionToEulerRV(&sensorValue.un.rotationVector, &ypr, true);
+    debugSerial.print(ypr.roll*-1);                       debugSerial.print("\t"); //Elevation 
+    debugSerial.print(map(ypr.yaw, 180, -180, 0, 360));   debugSerial.println("\t"); //Azimuth
+    sensorAzimuthAngle = (ypr.roll*-1);
   }
+
+  azimuthPID.Compute();
+  servo1.write(outputAzimuthPID);
+
   if (stringComplete) {
     // Remove leading spaces and check the input
     inputString.trim();
@@ -155,14 +124,14 @@ void loop() {
         azimuth = inputString.toFloat(); // Convert the remaining string to a float
         Serial.print("AZ");
         Serial.println(azimuth, 1);
-        servo1.write(round(azimuth));
+        desiredAzimuthAngle = azimuth;
         debugSerial.print("Command:AZ");
         debugSerial.println(azimuth, 1);
       } else {
         Serial.print("AZ");
         Serial.println(azimuth, 1);
         debugSerial.print("AZ");
-        debugSerial.println(azimuth, 1);
+        debugSerial.println(sensorAzimuthAngle, 1);
       }
     } else if (inputString.startsWith("EL")) {
       // If "EL" is received
