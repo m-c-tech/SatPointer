edacb472456e3d5e89d711679336b55f7e5d56cd
m-c-tech <73830912+m-c-tech@users.noreply.github.com>
2025-01-23 00:42:39 +0800

    Added features, fixed bugs
    Stepper motor control for azimuth.
    PID controllers now operate on error and control output velocity of the servo and stepper motors.

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,50 @@
 #include <Arduino.h>
 #include <Servo.h>
-#include <SoftwareSerial.h>
 #include <Adafruit_BNO08x.h>
 #include <QuickPID.h>
+#include <AccelStepper.h>
 
-#define debugSerial_TX 17
-#define debugSerial_RX 18
+#define Serial2_TX 17
+#define Serial2_RX -1
 #define BNO08X_SDA 21
 #define BNO08X_SCL 22
 #define BNO08X_RESET -1
+#define EN_PIN           -1 // Enable
+#define DIR_PIN          19 // Direction
+#define STEP_PIN         18 // Step
 
 float azimuth = 0.0;  // Store the current azimuth
 float elevation = 0.0; // Store the current elevation
-String inputString = "";      // a String to hold incoming data
-bool stringComplete = false;  // whether the string is complete
+String inputString = "";      // A string to hold incoming data
 bool hasBNO08X = false;
 static const int servoPin = 16;
 float outputServoAngle = 0;
-float desiredAzimuthAngle = 45, sensorAzimuthAngle = 0, outputAzimuthPID = 0, Kp = 0.1, Ki = 0.8, Kd = 0;
+int servoInterval = 100; // In milliseconds
+int servoElevationAngle = 0;
+double previousServoMillis = 0;
 
-QuickPID azimuthPID(&sensorAzimuthAngle, &outputAzimuthPID, &desiredAzimuthAngle, Kp, Ki, Kd,  /* OPTIONS */
+// PID variables
+float sensorAzimuthAngle = 0, outputAzimuthPID = 0, desiredAzimuthAngle = 0, azimuthError = 0;
+float sensorElevationAngle = 0, outputElevationPID = 0, desiredElevationAngle = 45, elevationError = 0;
+float setpoint = 0;
+
+// PID constants
+float azKp = 8, azKi = 1, azKd = 0;
+float elKp = 0.5, elKi = 0, elKd = 0;
+
+
+// PID objects
+QuickPID azimuthPID(&azimuthError, &outputAzimuthPID, &setpoint, azKp, azKi, azKd,  /* OPTIONS */
                azimuthPID.pMode::pOnError,                   /* pOnError, pOnMeas, pOnErrorMeas */
                azimuthPID.dMode::dOnMeas,                    /* dOnError, dOnMeas */
                azimuthPID.iAwMode::iAwCondition,             /* iAwCondition, iAwClamp, iAwOff */
-               azimuthPID.Action::direct); 
+               azimuthPID.Action::reverse);
+            
+QuickPID elevationPID(&elevationError, &outputElevationPID, &setpoint, elKp, elKi, elKd,  /* OPTIONS */
+               azimuthPID.pMode::pOnError,                   /* pOnError, pOnMeas, pOnErrorMeas */
+               azimuthPID.dMode::dOnMeas,                    /* dOnError, dOnMeas */
+               azimuthPID.iAwMode::iAwCondition,             /* iAwCondition, iAwClamp, iAwOff */
+               azimuthPID.Action::reverse);
 
 struct euler_t {
   float yaw;
@@ -31,44 +52,44 @@ struct euler_t {
   float roll;
 } ypr;
 
+AccelStepper stepper1(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
+
 Servo servo1;
-EspSoftwareSerial::UART debugSerial;
 Adafruit_BNO08x  bno08x(BNO08X_RESET);
 sh2_SensorValue_t sensorValue;
 sh2_SensorId_t reportType = SH2_ROTATION_VECTOR;
-long reportIMUIntervalUs = 10000;
+long reportIMUIntervalUs = 100000;
 
 void setReports(sh2_SensorId_t reportType, long report_interval) {
-  debugSerial.println("Setting desired reports");
+  Serial2.println("Setting desired reports");
   if (!bno08x.enableReport(reportType, report_interval)) {
-    debugSerial.println("Could not enable stabilized remote vector");
+    Serial2.println("Could not enable stabilized remote vector");
   }
 }
 
 void setup() {
   Serial.begin(115200);
-  debugSerial.begin(38400, SWSERIAL_8N1, debugSerial_RX, debugSerial_TX, false);
-  if (!debugSerial) { // If the object did not initialize, then its configuration is invalid
-    Serial.println("Invalid EspSoftwareSerial pin configuration, check config"); 
-    while (1) { // Don't continue with invalid configuration
-      delay (1000);
-    }
-  } else {
-    debugSerial.println("Debug serial started");
-  }
+  Serial2.begin(115200, SERIAL_8N1, Serial2_RX, Serial2_TX, false);
   if (!bno08x.begin_I2C()) {
-    debugSerial.println("Failed to find BNO08x chip");
+    Serial2.println("Failed to find BNO08x chip");
   } else {
-    debugSerial.println("BNO08x started");
+    Serial2.println("BNO08x started");
     hasBNO08X = true;
     setReports(reportType, reportIMUIntervalUs);
   }
-  azimuthPID.SetOutputLimits(0, 180);
+  azimuthPID.SetOutputLimits(-1000, 1000);
   azimuthPID.SetSampleTimeUs(100000);
-  azimuthPID.SetTunings(Kp, Ki, Kd);
+  azimuthPID.SetTunings(azKp, azKi, azKd);
   azimuthPID.SetMode(azimuthPID.Control::automatic);
+  elevationPID.SetOutputLimits(-20, 20);
+  elevationPID.SetSampleTimeUs(100000);
+  elevationPID.SetTunings(elKp, elKi, elKd);
+  elevationPID.SetMode(elevationPID.Control::automatic);
   inputString.reserve(200);
-  servo1.attach(servoPin, Servo::CHANNEL_NOT_ATTACHED, 0, 180);
+  servo1.attach(servoPin, Servo::CHANNEL_NOT_ATTACHED, 0, 180);  
+  stepper1.setMaxSpeed(1000);
+  stepper1.setEnablePin(EN_PIN);
+  stepper1.setPinsInverted(true, true, false);
 }
 
 void quaternionToEuler(float qr, float qi, float qj, float qk, euler_t* ypr, bool degrees = false) {
@@ -97,81 +118,109 @@ void quaternionToEulerGI(sh2_GyroIntegratedRV_t* rotational_vector, euler_t* ypr
     quaternionToEuler(rotational_vector->real, rotational_vector->i, rotational_vector->j, rotational_vector->k, ypr, degrees);
 }
 
+void processEasycommCommands(String inputString) {
+  //Process Easycomm II rotator commands
+  //Easycomm II position command: AZnn.n ELnn.n UP000 XXX DN000 XXX\n
+  //Easycomm II query command: AZ EL \n
+  String param;                                           //Parameter value
+  int firstSpace;                                         //Position of the first space in the command inputString
+  int secondSpace;                                        //Position of the second space in the command inputString
+  if (inputString.startsWith("AZ") && (inputString.length() == 2)) {                         //Query command received
+    Serial.print("AZ");
+    Serial.println(sensorAzimuthAngle, 1);
+    Serial2.print("AZ");
+    Serial2.print(sensorAzimuthAngle, 1); Serial2.print("\t");                                          //Send the current Azimuth and Elevation
+  } else {
+    if (inputString.startsWith("AZ")) {                          //Position command received: Parse the inputString.
+      firstSpace = inputString.indexOf(' ');                     //Get the position of the first space
+      secondSpace = inputString.indexOf(' ', firstSpace + 1);    //Get the position of the second space
+      param = inputString.substring(2, firstSpace);              //Get the first parameter
+      desiredAzimuthAngle = param.toFloat();                            //Set the azSet value
+      Serial2.print("Command:AZ"); Serial2.print(param); Serial2.print("\t");
+      param = inputString.substring(firstSpace + 3, secondSpace);//Get the second parameter
+      desiredElevationAngle = param.toFloat();                            //Set the elSet value
+      Serial2.print("Command:EL"); Serial2.println(param);
+    }
+  }
+  if (inputString.startsWith("EL") && (inputString.length() == 2)) {                         //Query command received
+    Serial.print("EL");
+    Serial.println(sensorElevationAngle, 1);
+    Serial2.print("EL");
+    Serial2.println(sensorElevationAngle, 1);                                          //Send the current Azimuth and Elevation
+  }
+  inputString = "";
+}
+
+float computeShortestAngleDifference(float targetAngle, float currentAngle) {
+  float difference = targetAngle - currentAngle;
+
+  // Wrap the difference to the range -180 to 180
+  while (difference > 180) difference -= 360;
+  while (difference < -180) difference += 360;
+
+  return difference;
+}
+
+void processCommands(void) {
+  //Process incoming data from the control computer
+  //User commands are entered by the user and are terminated with a carriage return
+  //Easycomm commands are generated by a tracking program and are terminated with a line feed
+  while (Serial.available()) {
+    char ch = Serial.read();                                //Read a single character from the serial buffer
+    switch (ch) {
+      case 13:                                                  //Carriage return received
+        //processUserCommands(inputString);                              //Process user commands
+        inputString = "";                                              //Command processed: Clear the command line
+        break;
+      case 10:                                                  //Line feed received
+        processEasycommCommands(inputString);                          //Process Easycomm commands
+        inputString = "";                                              //Command processed: Clear the command line
+        break;
+      default:                                                  //Any other character received
+        inputString += ch;                                             //Add this character to the command line
+        break;
+    }
+  }
+}
+
+void readSensorData() {
+  quaternionToEulerRV(&sensorValue.un.rotationVector, &ypr, true);
+  sensorAzimuthAngle = map(ypr.yaw * -1, -180, 180, 0, 360);
+  sensorElevationAngle = (ypr.pitch * -1);
+}
+
 void loop() {
   if (bno08x.wasReset()) {
-    debugSerial.print("sensor was reset ");
+    Serial2.print("sensor was reset ");
     setReports(reportType, reportIMUIntervalUs);
   }
-
   if (bno08x.getSensorEvent(&sensorValue)) {
-    quaternionToEulerRV(&sensorValue.un.rotationVector, &ypr, true);
-    debugSerial.print(ypr.roll*-1);                       debugSerial.print("\t"); //Elevation 
-    debugSerial.print(map(ypr.yaw, 180, -180, 0, 360));   debugSerial.println("\t"); //Azimuth
-    sensorAzimuthAngle = (ypr.roll*-1);
+    readSensorData();
   }
 
+  processCommands();
+
+  azimuthError = computeShortestAngleDifference(desiredAzimuthAngle, sensorAzimuthAngle);
   azimuthPID.Compute();
-  servo1.write(outputAzimuthPID);
-
-  if (stringComplete) {
-    // Remove leading spaces and check the input
-    inputString.trim();
-    if (inputString.startsWith("AZ")) {
-      // If "AZ" is received
-      inputString.remove(0, 2); // Remove "AZ" from the start
-      // Check if there's a float number following "AZ"
-      if (inputString.length() > 0) {
-        azimuth = inputString.toFloat(); // Convert the remaining string to a float
-        Serial.print("AZ");
-        Serial.println(azimuth, 1);
-        desiredAzimuthAngle = azimuth;
-        debugSerial.print("Command:AZ");
-        debugSerial.println(azimuth, 1);
-      } else {
-        Serial.print("AZ");
-        Serial.println(azimuth, 1);
-        debugSerial.print("AZ");
-        debugSerial.println(sensorAzimuthAngle, 1);
-      }
-    } else if (inputString.startsWith("EL")) {
-      // If "EL" is received
-      inputString.remove(0, 2); // Remove "EL" from the start
-      
-      // Check if there's a float number following "EL"
-      if (inputString.length() > 0) {
-        elevation = inputString.toFloat(); // Convert the remaining string to a float
-        Serial.print("EL");
-        Serial.println(elevation, 1);
-        debugSerial.print("Command:EL");
-        debugSerial.println(elevation, 1);
-      } else {
-        Serial.print("EL");
-        Serial.println(elevation, 1);
-        debugSerial.print("EL");
-        debugSerial.println(elevation, 1);
-      }
-    } else {
-      // If the input doesn't match AZ or EL commands
-      debugSerial.println("Unknown command:");
-      debugSerial.println(inputString);
-    }
-    
-    // Reset the inputString for the next command
-    inputString = "";
-    stringComplete = false;
+  stepper1.setSpeed(outputAzimuthPID);
+  stepper1.runSpeed();
+  //Serial2.print("sensorAzimuthAngle:");   Serial2.print(ypr.yaw*-1);       Serial2.print("\t"); //Azimuth
+  //Serial2.print("outputAzimuthPID:"); Serial2.print(outputAzimuthPID); Serial2.print("\t");
+  //Serial2.print("azimuthError"); Serial2.print(azimuthError); Serial2.println("\t");
+  
+  if (millis() - previousServoMillis > servoInterval) {
+    elevationError = computeShortestAngleDifference(desiredElevationAngle, sensorElevationAngle);
+    elevationPID.Compute();
+    servoElevationAngle = constrain(servoElevationAngle + outputElevationPID, 0 , 90);
+    servo1.write(servoElevationAngle);
+    previousServoMillis = millis();
   }
+
+  //Serial2.print("elevationError:"); Serial2.print(elevationError);  Serial2.print("\t"); //Elevation 
+  //Serial2.print("desiredElevationAngle:"); Serial2.print(desiredElevationAngle); Serial2.print("\t");
+  //Serial2.print("outputElevationPID:"); Serial2.println(outputElevationPID); Serial2.print("\t");
+  //Serial2.print("servoElevationAngle:"); Serial2.print(servoElevationAngle); Serial2.print("\t");
 }
 
-void serialEvent() {
-  while (Serial.available()) {
-    // get the new byte:
-    char inChar = (char)Serial.read();
-    // add it to the inputString:
-    inputString += inChar;
-    // if the incoming character is a newline, set a flag so the main loop can
-    // do something about it:
-    if (inChar == '\n') {
-      stringComplete = true;
-    }
-  }
-}
+
+
